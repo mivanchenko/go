7cbb63ee58243fec6b9da3c41c8acf20a26177bb
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2018-02-17 18:20:37 +0200

    Add more stuff re types

--- types.go
+++ types.go
@@ -2,17 +2,73 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/cmplx"
 )
 
 var (
-	ToBe bool     = false
-	MaxInt uint64 = 1 << 64 - 1
-	z complex128  = cmplx.Sqrt(-5 + 12i)
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
+
+const Pi = 3.14
+
+const (
+	// Create a huge number by shifting a 1 bit left 100 places.
+	Big = 1 << 100
+	// Shift it right again 99 places, so we end up with 1<<1, or 2.
+	Small = Big >> 99
+)
+
+func needInt(x int) int {
+	return x * 10 + 1
+}
+
+func needFloat(x float64) float64 {
+	return x * 0.1
+}
 
 func main() {
 	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
 	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
 	fmt.Printf("Type: %T Value: %v\n", z, z)
+
+	//	var i int
+	//	var f float64
+	//	var b bool
+	//	var s string
+	//	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+
+	var i int = 42
+	var f float64 = float64(i)
+	var u uint = uint(f)
+
+	//	i := 42
+	//	f := i
+	//	u := f
+
+	fmt.Printf("%v %v %v\n", i, f, u)
+
+	var x, y int = 3, 4
+	var f2 float64 = math.Sqrt(float64(x*x + y*y))
+	var z uint = uint(f2)
+
+	fmt.Println(x, y, f2, z)
+
+//	v := 42.0 + 0.5i // change me!
+	v := uint(25.0) // change me!
+	fmt.Printf("v is of type %T\n", v)
+
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "Day")
+
+	const Truth = true
+	fmt.Println("Go rules?", Truth)
+
+	fmt.Println(needInt(Small))
+	fmt.Println(needFloat(Small))
+	fmt.Println(needFloat(Big))
+//	fmt.Println(needInt(Big))
 }
